a1e2981104a203d5fa376ee511f1997482c114de
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-01-05 18:05:33 +0100

    Created main.lua

---
+++ main.lua
@@ -0,0 +1,40 @@
+local pluginName = "AyzeLYC Money System"
+local pluginAuthor = "AyzeLYC | AyzeLYC#4168"
+local pluginVersion = "1.0.0.0"
+local pluginRepository = ""
+
+local shop = {
+     
+     local minerals = {
+          
+          local diamond = {
+               
+               local diamondPrice = ""
+               local diamondNumberGived = "1"
+               local diamondItemId = "56"
+               
+          }
+          
+          local gold = {
+               
+               local goldPrice = ""
+               local goldNumberGived = "1"
+               local goldItemId = ""
+               
+          }
+          
+     }
+     
+}
+
+function shopCLI()
+     
+     
+     
+end
+
+function shopGUI()
+     
+     
+     
+end
