d48f591800285fa3ec6a97e9719b8fe39f966478
AyzeGaming <35877385+AyzeGamingFR@users.noreply.github.com>
2021-01-06 15:01:25 +0100

    Updated main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,21 @@
 local pluginName = "AyzeLYC Money System"
 local pluginAuthor = "AyzeLYC | AyzeLYC#4168"
-local pluginVersion = "1.0.0.1"
+local pluginVersion = "1.0.0.2"
 local pluginRepository = "https://github.com/AyzeGamingFR/cuberite-MoneySystem-plugin"
+
+local plugin = null
 
 local commands = {
      
      "/msmoney": {
           
-          
+          "commandText": ""
           
      }
      
      "/msshop": {
           
-          
+          "commanText": ""
           
      }
      
@@ -52,5 +54,25 @@ end
 function shopGUI()
      
      
+     
+end
+
+function Initialize(plugin)
+     
+     Plugin:SetAuthor(pluginAuthor)
+     Plugin:SetName(pluginName)
+     Plugin:SetVersion(pluginVersion)
+     plugin = Plugin
+     
+     LOG("Initialized the plugin " .. plugin:GetName .. " made by " .. plugin:getAuthor .. " version of the plugin " .. plugin:GetVersion)
+     return true
+     
+     cPluginManager.AddHook(cPluginManager.HOOK_PLAYER_JOINING, OnPlayerJoining)
+     
+end
+
+function OnDisable()
+     
+     LOG("The plugin " .. plugin:GetName .. " is being shutted down !")
      
 end
